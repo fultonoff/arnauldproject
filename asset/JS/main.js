558bdcb64268b6d494d2aa22660bda2a5543c723
fulton <fultonkilebe7@gmail.com>
2021-06-03 12:07:13 +0300

    navbar

---
+++ asset/JS/main.js
@@ -0,0 +1,40 @@
+/////////////////////////////
+//variables
+
+//toggle dropdown
+const dropdownBtn1 = document.querySelector('.dropdown-btn1')
+const dropdownBtn2 = document.querySelector('.dropdown-btn2')
+
+const dropContent1 = document.querySelector('.dropdown-content')
+const dropContent2 = document.querySelector('.dropdown-content2')
+
+///togglebtn
+const togBtn = document.querySelector('.toggle')
+const navList = document.querySelector('.nav-bar__list')
+
+
+//functions
+const dropToggler1 = ()=>{
+    dropContent1.classList.toggle('active')
+}
+
+const dropToggler2 = ()=>{
+    dropContent2.classList.toggle('active')
+}
+
+//init()
+dropdownBtn1.addEventListener('click', ()=>{
+    dropToggler1()
+})
+
+dropdownBtn2.addEventListener('click', ()=>{
+    dropToggler2()
+})
+
+//////////toggle nav
+
+togBtn.addEventListener('click', ()=>{
+    navList.classList.toggle('active')
+    togBtn.classList.toggle('active')
+})
+
