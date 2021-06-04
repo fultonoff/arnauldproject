6224850328fa29997a82a35d52b78c3b42da4e7a
fulton <fultonkilebe7@gmail.com>
2021-06-04 03:15:09 +0300

    feature

--- asset/JS/main.js
+++ asset/JS/main.js
@@ -2,39 +2,40 @@
 //variables
 
 //toggle dropdown
-const dropdownBtn1 = document.querySelector('.dropdown-btn1')
-const dropdownBtn2 = document.querySelector('.dropdown-btn2')
+const dropdownBtn1 = document.querySelector('.dropdown-btn1');
+const dropdownBtn2 = document.querySelector('.dropdown-btn2');
 
-const dropContent1 = document.querySelector('.dropdown-content')
-const dropContent2 = document.querySelector('.dropdown-content2')
+const dropContent1 = document.querySelector('.dropdown-content');
+const dropContent2 = document.querySelector('.dropdown-content2');
 
 ///togglebtn
-const togBtn = document.querySelector('.toggle')
-const navList = document.querySelector('.nav-bar__list')
-
+const togBtn = document.querySelector('.toggle');
+const navList = document.querySelector('.nav-bar__list');
 
 //functions
-const dropToggler1 = ()=>{
-    dropContent1.classList.toggle('active')
-}
+const dropToggler1 = () => {
+  dropContent1.classList.toggle('active');
+};
 
-const dropToggler2 = ()=>{
-    dropContent2.classList.toggle('active')
-}
+const dropToggler2 = () => {
+  dropContent2.classList.toggle('active');
+};
 
 //init()
-dropdownBtn1.addEventListener('click', ()=>{
-    dropToggler1()
-})
+dropdownBtn1.addEventListener('click', () => {
+  dropToggler1();
+});
 
-dropdownBtn2.addEventListener('click', ()=>{
-    dropToggler2()
-})
+dropdownBtn2.addEventListener('click', () => {
+  dropToggler2();
+});
 
 //////////toggle nav
 
-togBtn.addEventListener('click', ()=>{
-    navList.classList.toggle('active')
-    togBtn.classList.toggle('active')
-})
+togBtn.addEventListener('click', () => {
+  navList.classList.toggle('active');
+  togBtn.classList.toggle('active');
+});
 
+
+// gsap.from('.header', { duration: 0.6, ease: 'power2. out', opacity:0, y: -60, delay: .2});
